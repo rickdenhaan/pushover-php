14d4b2d881401da7282aedc3586b51445411580b
Rick den Haan <rick@capirussa.nl>
2014-06-20 20:26:31 +0200

    Create Exception.php

---
+++ Exception.php
@@ -0,0 +1,12 @@
+<?php
+namespace Capirussa\Pushover;
+
+/**
+ * Pushover Exception is thrown whenever a message cannot be sent to the Pushover API.
+ *
+ * @package Capirussa\Pushover
+ */
+class Exception extends \Exception
+{
+
+}
